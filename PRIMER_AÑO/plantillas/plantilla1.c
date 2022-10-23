eeaf0ebe11901b323430719fc314e87b22426c6f
Funes Tobias <tobiasfunes@hotmail.com.ar>
2022-10-23 12:25:53 -0300

    actualizando archivo plantillas de C

--- PRIMER_AÑO/plantillas/plantilla1.c
+++ PRIMER_AÑO/plantillas/plantilla1.c
@@ -6,11 +6,15 @@
 
 /* Estructuras */
 
+/* Declaracion  de tipo definido por el usuario */
+
 /* Prototipo de Funciones */
 
 /* Funcion principal main */
 int main(int argc, char const *argv[])
 {
+    /* Declaracion e inicializacion de variables */
+
     /* code */
     return 0;
 } // main
