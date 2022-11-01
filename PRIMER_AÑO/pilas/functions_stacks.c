15db33cf16a7938b3d9974f56cc1d2d2fe5794c5
Funes Tobias <tobiasfunes@hotmail.com.ar>
2022-10-31 22:54:51 -0300

    add new files

--- PRIMER_AÑO/pilas/functions_stacks.c
+++ PRIMER_AÑO/pilas/functions_stacks.c
@@ -1,15 +1,76 @@
 #include "pila_int.h"
 #include "functions_stacks.h"
 
-void addNode (struct node **stack, struct node **node)
+/* Funciones para Stacks */
+void addNode(struct node **stack, struct node **node)
 {
+    /* Creando memoria */
     *node = (struct node *)malloc(sizeof(struct node));
+    /* Verificacion de memoria disponible */
     if (*node == NULL)
     {
         printf("\nNo hay memoria disponible!\n");
-        exit(0);
+        exit(1); // finaliza la ejecucion
     }
+    /* El siguiente al nuevo nodo apunta al stackptr actual */
     (*node)->next = *stack;
+    /* El nuevo nodo es el nuevo stackptr */
     *stack = *node;
-    printf("exitoso");
+    // printf("\nCreacion de noodo exitosa!\n");
 } // addNode
+void popNode(struct node **stack)
+{
+    struct node *temp = NULL;
+    /* Verificamos que la stack no este vacia */
+    if (*stack != NULL)
+    {
+        /* Asignamos en temp el stackptr actual */
+        temp = *stack;
+        /* Al stackptr, se asigna el valor del siguiente nodo */
+        *stack = (*stack)->next;
+        /* Libera memoria ocupada por el primer nodo */
+        free(temp);
+    }
+    else
+    {
+        printf("\nPila Vacia\n");
+    }
+} // popNode
+void printStackInt(struct node *stack)
+{
+    struct node *temp = NULL;
+    /* Se comienza a recorrer desde el stackptr */
+    temp = stack;
+    while (temp != NULL)
+    {
+        printf("%d\t", temp->data);
+        /* asignamos el nodo sig. de temp a temp */
+        temp = temp->next;
+    }
+    printf("\n");
+} // printStackInt
+void freeMemory(struct node **stack)
+{
+    struct node *temp = NULL;
+    while (*stack != NULL)
+    {
+        temp = *stack;
+        *stack = (*stack)->next;
+        free(temp);
+    }
+} // freeMemory
+float addValues(struct node *stack)
+{
+    float total = 0;
+    struct node *temp = NULL;
+    /* Se comienza a recorrer desde el stackptr */
+    temp = stack;
+    while (temp != NULL)
+    {
+        /* Sumamos cada valor a la var total */
+        total += temp->data;
+        /* Se comienza a recorrer desde el stackptr */
+        temp = temp->next;
+    }
+    return total;
+} // addValues
--- PRIMER_AÑO/pilas/functions_stacks.h
+++ PRIMER_AÑO/pilas/functions_stacks.h
@@ -2,8 +2,12 @@
 #define FUNCTIONS_STACKS_H
 #include "pila_int.h"
 
-/* Funcion para añadir un nodo a la pila */
-void addNode (struct node **stack, struct node **node);
+/* Lista de Funciones para Pilas */
+void addNode(struct node **stack, struct node **node); // funcion para crear un nuevo nodo de Stack
+void popNode(struct node **stack); // funcion para eliminar el ultimo nodo de la pila
+void printStackInt(struct node *stack); // funcion para imprmir una Stack de enteros
+void freeMemory(struct node **stack); // libera la memoria creada por la lista
+float addValues(struct node *stack); // sumar los valores de la Stack
 
 #include "functions_stacks.c"
 #endif
