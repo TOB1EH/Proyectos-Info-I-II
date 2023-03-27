95b4a4b020fe3e3057d933557261c6b432e9ee4f
Tobias <tobiasfunes@hotmail.com.ar>
2023-03-27 00:34:19 -0300

    add ejercicies

---
+++ SEGUNDO_AÑO/Java/Ejercicios_01/src/EjercicioUno.java
@@ -0,0 +1,15 @@
+/**
+ * @author Tobias
+ */
+
+/* Ejercicio:  */
+
+
+// Librerias
+
+public class EjercicioUno {
+    public static void main(String[] args) {
+        int numero = 10;
+        System.out.println("Numero: "+numero);
+    }
+}
